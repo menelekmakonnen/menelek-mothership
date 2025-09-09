d6f4082a80d706b3e72fb1464b941147b27bb3e0
menelekmakonnen <hello@menelekmakonnen.com>
2025-09-09 15:59:03 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import dynamic from 'next/dynamic';
+import '../styles/globals.css';
+
+// Dynamically import the heavy AppShell component without SSR to avoid canvas issues during build.
+const AppShell = dynamic(() => import('../components/AppShell'), { ssr: false });
+
+export default function Home() {
+  return <AppShell />;
+}
