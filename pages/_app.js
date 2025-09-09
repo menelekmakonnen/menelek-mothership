7001a3205d8018c9223a6d62f72a1503ef84b300
menelekmakonnen <hello@menelekmakonnen.com>
2025-09-09 15:58:20 -0700

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+
+/**
+ * Custom App component to load global styles and render pages.
+ */
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
