49e37e2d9fcb478cf43666384f71d3eec434d2c7
menelekmakonnen <hello@menelekmakonnen.com>
2025-10-16 17:46:50 +0100

    Delete pages/_app.js

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css';
-
-/**
- * Custom App component to load global styles and render pages.
- */
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
