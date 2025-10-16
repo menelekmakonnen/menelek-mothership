13bd76954e18c06d4ddec8b9cb1063b412f98fff
menelekmakonnen <hello@menelekmakonnen.com>
2025-10-16 17:47:38 +0100

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-
-// Dynamically import the heavy AppShell component without SSR to avoid canvas issues during build.
-const AppShell = dynamic(() => import('../components/AppShell'), { ssr: false });
-
-export default function Home() {
-  return <AppShell />;
-}
