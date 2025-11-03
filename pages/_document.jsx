67c97e42284f60af874a5db670428cf619ee0a3e
menelekmakonnen <hello@menelekmakonnen.com>
2025-11-03 01:45:35 -0800

    Implement dynamic favicon switching

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,13 +4,26 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" id="favicon" href="/favicon_active.png" type="image/png" />
         <meta name="description" content="Menelek Makonnen - Filmmaker, Worldbuilder, AI Innovator" />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              document.addEventListener('visibilitychange', function() {
+                var favicon = document.getElementById('favicon');
+                if (document.hidden) {
+                  favicon.href = '/favicon_idle.png';
+                } else {
+                  favicon.href = '/favicon_active.png';
+                }
+              });
+            `,
+          }}
+        />
       </body>
     </Html>
   );
